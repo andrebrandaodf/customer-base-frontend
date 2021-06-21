4d59838c9d602f9153060a1ffff4948856b14ce1
andrebrandaodf <andrebrandaodf@gmail.com>
2021-06-21 11:47:23 -0300

    Adjusting

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -1,38 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProductComponent } from '../product/product.component';
-import { ProductDeleteComponent } from '../product-delete/product-delete.component';
-import { ProductReadComponent } from '../product-read/product-read.component';
-import { ProductUpdateComponent } from '../product-update/product-update.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {
+  MatSidenavContainer,
+  MatSidenavModule,
+} from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MediaVideoCrudComponent } from 'src/app/views/media-video-crud/media-video-crud.component';
 import { ProductCrudComponent } from 'src/app/views/product-crud/product-crud.component';
-import { MediaVideoComponent } from '../media-video/media-video.component';
+import { BlogComponent } from '../blog/blog.component';
 import { MediaVideoDeleteComponent } from '../media-video-delete/media-video-delete.component';
 import { MediaVideoReadComponent } from '../media-video-read/media-video-read.component';
 import { MediaVideoUpdateComponent } from '../media-video-update/media-video-update.component';
-import { MediaVideoCrudComponent } from 'src/app/views/media-video-crud/media-video-crud.component';
-import { NavbarComponent } from '../template/navbar/navbar.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {
-  MatSidenavModule,
-  MatSidenavContainer,
-} from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { CommonModule } from '@angular/common';
-import { AdminComponent } from './admin.component';
+import { MediaVideoComponent } from '../media-video/media-video.component';
+import { ProductDeleteComponent } from '../product-delete/product-delete.component';
+import { ProductReadComponent } from '../product-read/product-read.component';
+import { ProductUpdateComponent } from '../product-update/product-update.component';
+import { ProductComponent } from '../product/product.component';
 import { FooterComponent } from '../template/footer/footer.component';
-import { RouterModule } from '@angular/router';
-import { AdminRoutingModule } from './admin-routing.module';
 import { HeaderComponent } from '../template/header/header.component';
-import { BlogComponent } from '../blog/blog.component';
+import { NavbarComponent } from '../template/navbar/navbar.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+
+import { CKEditorModule } from 'ng2-ckeditor';
 
 @NgModule({
   declarations: [
@@ -69,6 +70,7 @@ import { BlogComponent } from '../blog/blog.component';
     MatPaginatorModule,
     MatSortModule,
     ReactiveFormsModule,
+    CKEditorModule,
   ],
   providers: [MatSidenavContainer],
 })
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CKEditorComponent } from 'ng2-ckeditor';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  ckeditorContent: string = '<b>Probando contenido</b>';
+  @ViewChild(CKEditorComponent) ckEditor!: CKEditorComponent;
+
+  ngAfterViewChecked() {
+    let editor = this.ckEditor.instance;
+    editor.config.height = '400';
+    editor.config.toolbarGroups = [
+      { name: 'document', groups: ['mode', 'document', 'doctools'] },
+      { name: 'clipboard', groups: ['clipboard', 'undo'] },
+      {
+        name: 'editing',
+        groups: ['find', 'selection', 'spellchecker', 'editing'],
+      },
+      {
+        name: 'paragraph',
+        groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'],
+      },
+      { name: 'insert', groups: ['insert'] },
+    ];
+    editor.config.removeButtons =
+      'Source, Save,Templates,Find,Replace,Scayt,SelectAll,Form,Radio';
+  }
 }
