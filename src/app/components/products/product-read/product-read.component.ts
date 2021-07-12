ed4b83741def9e574614093c04ad513c2cc29226
andrebrandaodf <andrebrandaodf@gmail.com>
2021-07-12 19:26:09 -0300

    Creating paginator and filter

--- src/app/components/products/product-read/product-read.component.ts
+++ src/app/components/products/product-read/product-read.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-product-read',
@@ -11,8 +13,10 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductReadComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  isLoading: boolean = true;
 
-  product: Product[] = [];
+  dataSource!: MatTableDataSource<any>;
+
   displayedColumns = [
     'name',
     'presentation',
@@ -21,20 +25,37 @@ export class ProductReadComponent implements OnInit {
     'action',
   ];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
+    this.dataSource = new MatTableDataSource();
+  }
 
   ngOnInit(): void {
-    console.log('entrou');
-    this.productService.getAll().subscribe((product: any) => {
-      console.log(product);
-      this.product = product;
-    });
-    this.productService.read().subscribe((product: any) => {
-      this.product = product;
-      console.log(product);
-    });
+    this.getAllPaginator();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getAllPaginator() {
+    this.isLoading = true;
+    this.productService.getAll().subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource(res || []);
+      setTimeout(() => this.dataSource.paginator = this.paginator);
+      this.isLoading = false;
+    })
   }
 }
+
 
 export interface PeriodicElement {
   name: string;
